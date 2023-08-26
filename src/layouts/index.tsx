a829fe1747c36ceaca5b4cceee7002cfa91631d2
LuckyFBB <976060700@qq.com>
2023-08-26 14:47:21 +0800

    feat: add UI for add page

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,20 @@
 import { Outlet } from "umi";
 import styles from "./index.less";
 import "./reset.less";
+import zhCN from "antd/es/locale/zh_CN";
+import { ConfigProvider } from "antd";
 
 export default function Layout() {
     return (
-        <div className={styles.layout}>
-            <section className={styles.container}>
-                <Outlet />
-            </section>
-            <footer className={styles.footer}>
-                Copyright © 2023-{new Date().getFullYear()} Made by FBB 🤸‍♂️
-            </footer>
-        </div>
+        <ConfigProvider locale={zhCN}>
+            <div className={styles.layout}>
+                <section className={styles.container}>
+                    <Outlet />
+                </section>
+                <footer className={styles.footer}>
+                    Copyright © 2023-{new Date().getFullYear()} Made by FBB 🤸‍♂️
+                </footer>
+            </div>
+        </ConfigProvider>
     );
 }
--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -1,14 +1,38 @@
+import { Form, Input, Select } from "antd";
 import selectStyles from "../select/index.less";
 import styles from "./index.less";
 
+const Option = Select.Option;
+
 const Add = () => {
+    const [form] = Form.useForm();
+
+    const handleAdd = () => {
+        const values = form.getFieldsValue();
+    };
     return (
         <div className={selectStyles.container}>
             <span className={selectStyles.title}>添加菜品 🍝 </span>
             <div className={styles.container}>
-                <input></input>
-                <select></select>
-                <div>确认添加</div>
+                <Form form={form}>
+                    <Form.Item name="catalog">
+                        <Select
+                            placeholder="请选择菜品类别"
+                            allowClear
+                            className={styles.select}
+                        >
+                            <Option value={1}>蔬菜 🥬</Option>
+                            <Option value={2}>肉品 🥩</Option>
+                            <Option value={3}>汤品 🥣</Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="vegetable">
+                        <Input placeholder="请输入菜品名称" />
+                    </Form.Item>
+                </Form>
+                <div className={styles.button} onClick={handleAdd}>
+                    确认添加
+                </div>
             </div>
         </div>
     );
